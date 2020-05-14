e5ea4aa4545104223425869dbabb834a013f276f
BOUHSENrafiq <58845334+BOUHSENrafiq@users.noreply.github.com>
2020-05-14 01:04:48 +0000

    V1

---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Input } from '@angular/core';
+import { Output, EventEmitter } from '@angular/core';
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent implements OnInit {
+  /*
+  *property named product with an @Input() decorator.
+  *The @Input() decorator indicates that the property value passes in from the component's parent, the product list component.
+   */
+  @Input() product;
+  @Output() notify = new EventEmitter();
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/products.ts
@@ -0,0 +1,19 @@
+/* data */
+
+export const products = [
+  {
+    name: 'Phone 1',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    name: 'Phone 2',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    name: 'Phone 3',
+    price: 299,
+    description: ' Standard phone'
+  }
+];
